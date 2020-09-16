2e1a7ecc3257ed9806b2ba71cd0113667eded91d
Justin Choo <kchoosun@gmail.com>
2020-09-15 20:28:02 -0700

    deleted unnecessary files

--- client/index.js
+++ client/index.js
@@ -3,7 +3,6 @@ import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import styles from './App.css';
 
 render(
   <BrowserRouter>
